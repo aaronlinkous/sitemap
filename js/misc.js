3fa2b401a19cc9986e23c0cbf30f186b0342b341
Aaron <me@aaronlinkous.com>
2013-04-10 12:19:43 -0700

    basic click and dbl click actions

--- js/misc.js
+++ js/misc.js
@@ -3,11 +3,27 @@ function is_parent() {
 		$(this).next().is("ul") ? $(this).addClass("parent") : $(this).removeClass("parent");
 	});
 }
-
+var clicks = 0, timer, node;
 $(document).ready(function(){
 	is_parent();
 
-	$(".parent").on("click", function() {
-		$(this).toggleClass("closed");
+	$("li").on("dblclick", function(e) {
+		e.preventDefault();
+	});
+
+	$("li").on("click", function() {
+		clicks++;
+		node = $(this);
+
+		if(clicks === 1) {
+			timer = setTimeout(function() {
+				if(node.hasClass("parent")) node.toggleClass("closed");
+				clicks = 0;
+			}, 250);
+		} else {
+			clearTimeout(timer);
+			alert(node);
+			clicks = 0;
+		}
 	});
 });
